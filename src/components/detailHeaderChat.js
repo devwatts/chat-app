a28c7a35d98ef4d2f7870a58b2451ba7ed012d97
Dev Watts <devwatts5@gmail.com>
2022-02-14 14:29:36 +0530

    -

---
+++ src/components/detailHeaderChat.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default class DetailHeaderChat extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="header-parent"></div>
+        );
+    }
+}
